b87f5c24ff3f218176a96bab677a04675f6905ba
HP <HP@DESKTOP-IHTAUMA>
2023-04-29 15:09:19 +0700

    Add project files.

---
+++ Do-an-co-so/Models/navbarcategoryViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Do_an_co_so.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace Do_an_co_so.Models
+{
+    public class navbarcategoryViewComponent : ViewComponent
+    {
+        private readonly Do_an_co_soContext _context;
+
+        public navbarcategoryViewComponent(Do_an_co_soContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(_context.Categories.ToList());
+        }
+    }
+}
